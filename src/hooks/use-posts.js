57538bb22077d99417c7957850d727c65bc26c75
brittani1128 <brittani1128@gmail.com>
2020-07-18 21:09:09 -0500

    add posts to blog

---
+++ src/hooks/use-posts.js
@@ -0,0 +1,27 @@
+import { graphql, useStaticQuery } from 'gatsby';
+
+const usePosts = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      allMdx {
+        nodes {
+          frontmatter {
+            title
+            slug
+            author
+          }
+          excerpt
+        }
+      }
+    }
+  `);
+
+  return data.allMdx.nodes.map(({ frontmatter, excerpt }) => ({
+    title: frontmatter.title,
+    author: frontmatter.author,
+    slug: frontmatter.slug,
+    excerpt,
+  }));
+};
+
+export default usePosts;
