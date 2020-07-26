360212380a5ef77af69a5c021f5de4d17c524fa4
brittani1128 <brittani1128@gmail.com>
2020-07-26 15:48:59 -0500

    style fixes

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,7 +5,10 @@ import Layout from '../components/layout';
 export default () => (
   <Layout>
     <h1>About Me</h1>
-    <p>Welcome to my personal website</p>
+    <p>
+      Welcome to my practice website. This is my first gatsby site I've build
+      for a front end masters course.
+    </p>
     <Link to="/">Home</Link>
   </Layout>
 );
